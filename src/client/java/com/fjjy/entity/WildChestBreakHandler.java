e2bb6fcc5ed8e043e49c1b7342e6a95e31640e73
adam hecker <tfxflrstx@gmail.com>
2026-04-07 00:00:15 -0400

    smaller particles continuously while breaking

--- src/client/java/com/fjjy/entity/WildChestBreakHandler.java
+++ src/client/java/com/fjjy/entity/WildChestBreakHandler.java
@@ -1,21 +1,29 @@
 package com.fjjy.entity;
 
+import com.fjjy.config.FlyingChestClientConfig;
 import com.fjjy.config.FlyingChestServerConfig;
 import com.fjjy.network.WildChestBreakStatePayload;
 import com.fjjy.network.WildChestBreakStatePayload.State;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.TerrainParticle;
+
+import java.util.function.Function;
 
 public class WildChestBreakHandler {
 
     // -1 = no active break session. set to entity id while the crosshair is on a wild chest and attack is held
     private int activeBreakEntityId = -1;
+    private int digParticleTicks = 0;
+
+    private static final int DIG_PARTICLE_INTERVAL = 2;
 
     private void setActive(int entityId) {
         if (activeBreakEntityId == entityId) return;
         // Cancel any in-progress block mining when switching to a chest
-        var mc = net.minecraft.client.Minecraft.getInstance();
+        var mc = Minecraft.getInstance();
         if (mc.gameMode != null) mc.gameMode.stopDestroyBlock();
         if (activeBreakEntityId != -1) {
             ClientPlayNetworking.send(new WildChestBreakStatePayload(activeBreakEntityId, State.STOP));
@@ -28,6 +36,7 @@ public class WildChestBreakHandler {
         if (activeBreakEntityId == -1) return;
         ClientPlayNetworking.send(new WildChestBreakStatePayload(activeBreakEntityId, State.STOP));
         activeBreakEntityId = -1;
+        digParticleTicks = 0;
     }
 
     public void register() {
@@ -47,6 +56,21 @@ public class WildChestBreakHandler {
                 && client.player.distanceToSqr(wildChest) <= maxDistSqr
             ) {
                 setActive(wildChest.getId());
+
+                if (++digParticleTicks >= DIG_PARTICLE_INTERVAL) {
+                    digParticleTicks = 0;
+                    var hit = ehr.getLocation();
+                    var rand = client.level.getRandom();
+                    var blockState = FlyingChestClientConfig.INSTANCE.chestVariant.particleBlock.defaultBlockState();
+                    Function<Double, Double> spread = n -> n + (rand.nextDouble() - 0.5) * .3;
+                    
+                    client.particleEngine.add(
+                        new TerrainParticle(client.level,
+                            spread.apply(hit.x), spread.apply(hit.y), spread.apply(hit.z),
+                            0.5, 0.5, 0.5,
+                            blockState)
+                        .setPower(0.2F).scale(0.5F));
+                }
             } else {
                 stop();
             }
